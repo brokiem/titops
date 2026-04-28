8c7393b56eee78dca9afd3e49295580271303e8f
brokiem <milyhqvideo@gmail.com>
2026-04-28 23:05:29 +0800

    feat: enhance Vite configuration to load environment variables for API proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+	// Load env file based on `mode` in the current working directory.
+	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
-	server: {
-		proxy: {
-			"/api": process.env.API_URL!,
+		server: {
+			proxy: {
+				"/api": {
+					target: env.API_URL || "http://localhost:3000",
+					changeOrigin: true,
+				},
+			},
 		},
-	},
+	};
 });
