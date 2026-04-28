a62a7ce977be5043a33304590a1144547a2f2177
brokiem <milyhqvideo@gmail.com>
2026-04-28 22:15:14 +0800

    feat: refactor server.ts to export app instead of starting server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,5 @@ import {getServerEnv} from "./config/env";
 const env = getServerEnv();
 
 const app = createApp({env});
-const server = Bun.serve({
-    hostname: "0.0.0.0",
-    port: env.PORT,
-    fetch: app.fetch,
-});
 
-console.log(`Server running on ${server.url}`);
+export default app;
